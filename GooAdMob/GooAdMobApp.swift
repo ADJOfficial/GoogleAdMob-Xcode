517d5c3c023fec2664bddb891182a9bc99e881a8
ADJOfficial <ArsalanDaud098@gamil.com>
2024-04-15 13:09:37 +0500

    Initial Commit

---
+++ GooAdMob/GooAdMobApp.swift
@@ -0,0 +1,17 @@
+//
+//  GooAdMobApp.swift
+//  GooAdMob
+//
+//  Created by ADJ on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GooAdMobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
